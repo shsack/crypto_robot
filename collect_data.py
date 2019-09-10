a3956ba55087d9946a4e331525af9699620a8ffc
ssack <ssack@student.ethz.ch>
2019-09-10 14:15:40 +0200

    Collect Bitcoin price data

---
+++ collect_data.py
@@ -0,0 +1,70 @@
+import requests
+from datetime import datetime
+import pandas as pd
+
+
+def get_filename(from_symbol, to_symbol, exchange, datetime_interval, download_date):
+    return '%s_%s_%s_%s_%s.csv' % (from_symbol, to_symbol, exchange, datetime_interval, download_date)
+
+
+def download_data(from_symbol, to_symbol, exchange, datetime_interval):
+    supported_intervals = {'minute', 'hour', 'day'}
+    assert datetime_interval in supported_intervals,\
+        'datetime_interval should be one of %s' % supported_intervals
+
+    print('Downloading %s trading data for %s %s from %s' %
+          (datetime_interval, from_symbol, to_symbol, exchange))
+    base_url = 'https://min-api.cryptocompare.com/data/histo'
+    url = '%s%s' % (base_url, datetime_interval)
+
+    params = {'fsym': from_symbol, 'tsym': to_symbol,
+              'limit': 2000, 'aggregate': 1,
+              'e': exchange}
+    request = requests.get(url, params=params)
+    data = request.json()
+    return data
+
+
+def convert_to_dataframe(data):
+    df = pd.io.json.json_normalize(data, ['Data'])
+    df['datetime'] = pd.to_datetime(df.time, unit='s')
+    df = df[['datetime', 'low', 'high', 'open',
+             'close', 'volumefrom', 'volumeto']]
+    return df
+
+
+def filter_empty_datapoints(df):
+    indices = df[df.sum(axis=1) == 0].index
+    print('Filtering %d empty datapoints' % indices.shape[0])
+    df = df.drop(indices)
+    return df
+
+
+def read_dataset(filename):
+    print('Reading data from %s' % filename)
+    df = pd.read_csv(filename)
+    df.datetime = pd.to_datetime(df.datetime)  # change type from object to datetime
+    df = df.set_index('datetime')
+    df = df.sort_index()  # sort by datetime
+    return df
+
+
+from_symbol = 'BTC'
+to_symbol = 'USD'
+exchange = 'Bitstamp'
+datetime_interval = 'day'
+
+data = download_data(from_symbol, to_symbol, exchange, datetime_interval)
+df = convert_to_dataframe(data)
+df = filter_empty_datapoints(df)
+df = df.rename(columns={'volumefrom': 'volume'})
+del df['volumeto']
+
+current_datetime = datetime.now().date().isoformat()
+filename = get_filename(from_symbol, to_symbol, exchange, datetime_interval, current_datetime)
+
+print('Saving data to %s' % filename)
+df.to_csv(filename, index=False)
+
+
+
