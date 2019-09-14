4510558392157861fd38e1874e9c68b950597399
ssack <ssack@student.ethz.ch>
2019-09-14 13:39:02 +0200

    put data pre and post-processing functions into module

--- data_collector/collect_data.py
+++ data_collector/collect_data.py
@@ -76,12 +76,26 @@ def collect_data(from_symbol, to_symbol, exchange, datetime_interval):
     df.to_csv(filename, index=False)
 
 
-from_symbol = 'BTC'
-to_symbol = 'USD'
-exchange = 'Bitstamp'
-datetime_interval = 'day'
+def series_to_supervised(data, n_in=1, n_out=1):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    agg.dropna(inplace=True)
 
-collect_data(from_symbol, to_symbol, exchange, datetime_interval)
-
+    return agg
 
 
